254e3b6280e85044138c9b02f2d5721bebb08420
Aria Amini <amini5454@gmail.com>
2026-05-23 19:20:34 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default {
 	},
 	test: {
 		passWithNoTests: true,
-		projects: ['packages/*'],
+		projects: ['packages/*/vite.config.ts'],
 		exclude: [
 			'**/node_modules/**',
 			'**/dist/**',
