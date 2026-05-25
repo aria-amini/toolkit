1175f274084b96754a6ac2ef9971ebdc1e6d36ed
Aria Amini <amini5454@gmail.com>
2026-05-25 00:46:29 -0400

    add visual tests

--- packages/config/vite.config.ts
+++ packages/config/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite-plus'
 
 export default defineConfig({
 	pack: {
-		entry: ['src/**/*.ts', '!src/workspace.ts', '!src/plugins/**/*.ts'],
+		entry: ['src/**/*.ts', 'src/**/*.tsx', '!src/workspace.ts', '!src/plugins/**/*.ts'],
 		dts: true,
 		format: ['esm'],
 		sourcemap: true,
